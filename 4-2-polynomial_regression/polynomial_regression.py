974d42c4e462d3b2c46b218553d40c40a74955b1
Tobias Doernbach <t.doernbach@cellumation.com>
2020-11-06 11:31:27 +0100

    bugfix: proper train/test splitting for polynomial regression

--- 4-2-polynomial_regression/polynomial_regression.py
+++ 4-2-polynomial_regression/polynomial_regression.py
@@ -17,8 +17,13 @@ amazon_y = dataset[:, np.newaxis, 1]
 # generate plot with input data
 fig = px.bar(x=amazon_X.flatten(), y=amazon_y.flatten())
 
-# TODO split the data into training/testing sets
-amazon_X_train, amazon_X_test, amazon_y_train, amazon_y_test = ...
+# split the data into training/testing sets - use every third value for testing
+amazon_X_test = amazon_X[::3]
+amazon_X_train = [x for x in amazon_X if x not in amazon_X_test]
+
+# split the targets into training/testing sets - use every third value for testing
+amazon_y_test = amazon_y[::3]
+amazon_y_train = [x for x in amazon_y if x not in amazon_y_test]
 
 # TODO add some more future x values to amazon_X_test to get predictions for (e.g. for 2021, 2022,...)
 amazon_X_test = ...
