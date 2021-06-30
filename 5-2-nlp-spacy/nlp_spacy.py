22de717fe25ce77f2f4b3d7cfd996792052fd719
Semjon Bibow <semjon.bibow@cgi.com>
2021-06-30 09:43:26 +0200

    My updates

--- 5-2-nlp-spacy/nlp_spacy.py
+++ 5-2-nlp-spacy/nlp_spacy.py
@@ -2,6 +2,8 @@
 import spacy
 import csv
 from datetime import datetime
+from pprint import pprint
+import matplotlib.pyplot as plt
 
 nlp = spacy.load("en_core_web_sm")
 
@@ -15,21 +17,37 @@ with open('trump_tweets_20201017.csv', newline='') as csvfile:
     for row in records:
         row_date = datetime.strptime(row['date'], "%Y-%m-%d %H:%M:%S")
         # TODO check if row_date is after EARLIEST_DATE
-        if ...:
+        if row_date > EARLIEST_DATE:
             # TODO check if text in row does not start with 'RT'
-            if ...:
+            if row['isRetweet'] == "f":
                 # TODO add this row to full_text
-                ...
+                full_text += row['text']
 
 # TODO run the actual Natural Language Processing (see exercise 5.2.3, simply call "nlp(...)" on the text)
-doc = ...
+doc = nlp(full_text)
 
 # print all mentioned persons
-for entity in doc.ents:
-    if entity.label_ == 'PERSON':
-        # TODO print this entry
-        ...
+# for entity in doc.ents:
+#     if entity.label_ == 'PERSON':
+#         # TODO print this entry
+#         print(entity)
 
 # TODO count the number of 'joe' and 'biden' in the tweets
-num_bidens = ...
-print('Number of Bidens in Trump\'s tweets since ' + str(EARLIEST_DATE) + ': ' + str(num_bidens))
+def filter_bidens(text):
+    return "joe" in str(text).lower() or "biden" in str(text).lower()
+
+bidens = list(filter(filter_bidens, doc.ents))
+print('Number of Bidens in Trump\'s tweets since ' + str(EARLIEST_DATE) + ': ' + str(len(bidens)))
+
+labels = {}
+texts = {}
+for ent in bidens:
+    labels[ent.label_] = labels.get(ent.label_,0)+1
+    texts[str(ent)] = texts.get(str(ent),0)+1
+pprint(labels)
+most_used_bidens = sorted(texts, key=lambda e: texts[e], reverse=True)[:10]
+most_used_bidens_hist = {text: texts[text] for text in most_used_bidens}
+
+plt.bar(most_used_bidens_hist.keys(), most_used_bidens_hist.values())
+plt.show()
+
